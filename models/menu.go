7b971a71373b58b1fad96204fba380ee85312ae0
teeratchapat <teeratchapat.c@kkumail.com>
2024-09-27 20:43:15 +0700

    feat:set up project

--- models/menu.go
+++ models/menu.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type Menu struct {
 	gorm.Model
-	Name       string `json:"name"`                                // ชื่อของเมนู
-	Route      string `json:"route"`                               // เส้นทางของเมนู
-	Icon       string `json:"icon"`                                // ไอคอนของเมนู
-	IsChildren bool   `json:"is_children"`                         // บ่งบอกว่าเป็นเมนูย่อยหรือไม่
-	ParentID   *uint  `json:"parent_id"`                           // ถ้าเป็นเมนูย่อยจะอ้างถึงเมนูหลัก
-	Children   []Menu `json:"children" gorm:"foreignKey:ParentID"` // เมนูย่อย (ถ้ามี)
+	Name       string `json:"name"`
+	Route      string `json:"route"`
+	Icon       string `json:"icon"`
+	IsChildren bool   `json:"is_children"`
+	ParentID   *uint  `json:"parent_id"`
+	Children   []Menu `json:"children" gorm:"foreignKey:ParentID"`
 }
